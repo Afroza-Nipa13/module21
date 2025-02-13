a91c6609b06b4957a4281c5a2a40aadfaacc7b0b
unknown <you@examp>
2025-02-13 10:47:37 +0600

    first commit

---
+++ perameter.js
@@ -0,0 +1,37 @@
+const vaatKhaw(){
+    console.log('hand wash kore aso');
+    console.log('bosho');
+    console.log('khabar naw');
+    console.log('khaw');
+}
+vaatKhaw();
+
+function square (number){
+    console.log('value of the number', number);
+    const borgo = number * number;
+    console.log('square of the number', borgo);
+}
+square(4);
+console.log('..............')
+square(12);
+square(5);
+square(120);
+
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    console.log(sum);
+
+}
+
+add(3, 8);
+add(30, 20);
+
+
+function addAll(a, b, c, d ){
+    const add = a+b+c+d;
+    console.log(add);
+
+}
+addAll(2,5,6);
+addAll(4,5,9,4);
