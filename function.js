738f5663c53850d880987b2dbb32f2e71979df88
unknown <you@examp>
2025-02-13 10:49:20 +0600

    added function return

---
+++ function.js
@@ -0,0 +1,14 @@
+const number =45;
+function fanOffKor(){
+    console.log('bosha theke uthe daraw');
+    console.log('walk towards the switch');
+    console.log('click the switch to off the fan');
+}
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('brush at least 2min and do gurgle');
+}
+
+fanOffKor();
+brushTeeth();
